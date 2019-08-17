2f9aed1f39d7142c7eae69bbfdd9a483ef7d9e75
geyoga <yogadewantama@gmail.com>
2019-08-17 22:38:25 +0700

    Initial Commit

---
+++ TextDetection-V1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextDetection-V1
+//
+//  Created by Georgius Yoga Dewantama on 17/08/19.
+//  Copyright © 2019 Georgius Yoga Dewantama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
